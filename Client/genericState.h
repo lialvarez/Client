cbd98682480a7b148d49126e6b88302200d7451f
lialvarez <lialvarez@itba.edu.ar>
2017-05-08 00:38:57 -0300

    chau me fui a dormir

--- Client/genericState.h
+++ Client/genericState.h
@@ -8,8 +8,8 @@ class genericState
 public:
 	// Estas funciones responden a los eventos recibidos en el estado actual. Por default, se toma que los eventos recibidos son eventos inesperados, lo que sse indica devolviendo nullptr. Al ser virtuales, las clases de los estados especificos que heredan a genericState pueden redefinir que respuesta tomar ante los eventos que no son inesperados para ese estado en especifico.
 	virtual genericState* on_NoEv(genericEvent* ev) { return nullptr; }
-	virtual genericState* on_Put(genericEvent* ev,) { return nullptr; }
-	virtual genericState* on_Get(genericEvent* ev) { return nullptr; }
+	virtual genericState* on_Put(genericEvent* ev, usefulInfo *Info) { return nullptr; }
+	virtual genericState* on_Get(genericEvent* ev, usefulInfo *Info) { return nullptr; }
 	virtual genericState* on_CloseClient(genericEvent* ev) { return nullptr; }
 	virtual genericState* on_Help(genericEvent* ev) { return nullptr; }
 	virtual genericState* on_ClearTerminal(genericEvent *ev) { return nullptr; }
@@ -17,12 +17,12 @@ public:
 	virtual genericState* on_FileError(genericEvent* ev) { return nullptr; }
 	virtual genericState* on_InvalidCommand(genericEvent* ev) { return nullptr; }
 	virtual genericState* on_Data(genericEvent* ev) { return nullptr; }
-	virtual genericState* on_Ack(genericEvent* ev) { return nullptr; }
+	virtual genericState* on_Ack(genericEvent* ev, usefulInfo *Info) { return nullptr; }
 	virtual genericState* on_Error(genericEvent* ev) { return nullptr; }
 	virtual genericState* on_WRQ(genericEvent* ev) { return nullptr; }
 	virtual genericState* on_RRQ(genericEvent *ev) { return nullptr; }
 	virtual genericState* on_ConnectionFailed(genericEvent* ev) { return nullptr; }
-	virtual genericState* on_LastData(genericEvent* ev) { return nullptr; }
+	virtual genericState* on_LastData(genericEvent* ev, usefulInfo *Info) { return nullptr; }
 	virtual genericState* on_Timeout(genericEvent* ev) { return nullptr; }
 
 
